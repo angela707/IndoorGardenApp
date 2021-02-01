75e5b16f9dcf0d49b0afcbe1125518ae83678f8f
Angela Dimovska <angelaa.dimovska7@gmail.com>
2021-02-01 02:45:11 +0100

    Initial commit

---
+++ app/src/main/java/angela/example/indoorgardenapp/LogIn.java
@@ -0,0 +1,17 @@
+package angela.example.indoorgardenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+    }
+
+}
