67835e059e4ea39176485fc34ca407d29d3dcf31
Angela Dimovska <angelaa.dimovska7@gmail.com>
2021-02-02 04:00:22 +0100

    Napraven layout za cardview na plants

--- app/src/main/java/angela/example/indoorgardenapp/LogIn.java
+++ app/src/main/java/angela/example/indoorgardenapp/LogIn.java
@@ -2,15 +2,40 @@ package angela.example.indoorgardenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
+    Button login, register, guest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        login = findViewById(R.id.loginbtn);
+        register = findViewById(R.id.registerbtn);
+        guest = findViewById(R.id.guestbtn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
