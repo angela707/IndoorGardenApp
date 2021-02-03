599630a656cc01f3c232d38f80c69a57cf573a27
Angela Dimovska <angelaa.dimovska7@gmail.com>
2021-02-03 03:33:44 +0100

    Dodadeni ShoppingCart, Wishlist, FloatingButton za ShoppingCart

---
+++ app/src/main/java/angela/example/indoorgardenapp/ShoppingCartAdapter.java
@@ -0,0 +1,47 @@
+package angela.example.indoorgardenapp;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ShoppingCartAdapter extends RecyclerView.Adapter<ShoppingCartAdapter.ShoppingCartViewHolder>{
+
+    Context context;
+
+    public ShoppingCartAdapter (Context context){
+
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public ShoppingCartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_shopping_cart_row, parent, false);
+        ShoppingCartAdapter.ShoppingCartViewHolder holder = new ShoppingCartAdapter.ShoppingCartViewHolder(view);
+        return holder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ShoppingCartAdapter.ShoppingCartViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+
+    public class ShoppingCartViewHolder extends RecyclerView.ViewHolder{
+
+        public ShoppingCartViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+        }
+    }
+}
