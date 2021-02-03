d73bb15c925dae35da52ae1bc7cae9b7d9386531
Angela Dimovska <angelaa.dimovska7@gmail.com>
2021-02-03 03:33:30 +0100

    Dodadeni ShoppingCart, Wishlist, FloatingButton za ShoppingCart

--- app/src/main/java/angela/example/indoorgardenapp/PlantsActivity.java
+++ app/src/main/java/angela/example/indoorgardenapp/PlantsActivity.java
@@ -1,10 +1,19 @@
 package angela.example.indoorgardenapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PlantsActivity extends AppCompatActivity {
 
@@ -14,12 +23,45 @@ public class PlantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plants);
+        Toolbar myToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(myToolbar);
 
+        FloatingActionButton fab = findViewById(R.id.shopping_btn);
         myRecyclerView = findViewById(R.id.recyclerPlants);
 
         PlantsAdapter adapter = new PlantsAdapter(this);
         myRecyclerView.setAdapter(adapter);
         myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PlantsActivity.this, "KLIKNA NA SHOPPING", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(PlantsActivity.this, ShoppingCartActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.myWishlist){
+
+            Intent intent = new Intent (this, WishlistActivity.class);
+            startActivity(intent);
+        }
+        return true;
+
+    }
+
 }
--- app/src/main/java/angela/example/indoorgardenapp/ShoppingCartActivity.java
+++ app/src/main/java/angela/example/indoorgardenapp/ShoppingCartActivity.java
@@ -1,30 +1,22 @@
 package angela.example.indoorgardenapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class WishlistActivity extends AppCompatActivity {
-
+public class ShoppingCartActivity extends AppCompatActivity {
     RecyclerView myRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wishlist);
+        setContentView(R.layout.activity_shopping_cart);
 
-        myRecyclerView = findViewById(R.id.recyclerWishlist);
-
-        WishlistAdapter adapter = new WishlistAdapter(this);
+        myRecyclerView = findViewById(R.id.recyclerShoppingCart);
+        ShoppingCartAdapter adapter = new ShoppingCartAdapter(this);
         myRecyclerView.setAdapter(adapter);
         myRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
-
-
 }
